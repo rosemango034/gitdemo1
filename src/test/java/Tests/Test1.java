1e10d15384dd2d46d5c088a4eb220da017c23264
chena <chena@chenaganiswapna>
2023-08-14 10:36:39 +0530

    first commit.......

---
+++ src/test/java/Tests/Test1.java
@@ -0,0 +1,18 @@
+package Tests;
+
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		// 1. Make a Request
+		RestAssured.baseURI = "https://jsonplaceholder.typicode.com";
+		// 2. Submit request via HTTP method to get response
+		Response res = RestAssured.get("/posts/34"); // path parameter
+		// 3. Analyze response
+		String z = res.getBody().asString();
+		System.out.println(z);
+	}
+}
+	
