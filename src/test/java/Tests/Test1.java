ce2210cc468a6f6e0c8279648a80548e69dc6159
chena <chena@chenaganiswapna>
2023-08-15 10:46:04 +0530

    i am changing Test1 to Branchx

--- src/test/java/Tests/Test1.java
+++ src/test/java/Tests/Test1.java
@@ -11,8 +11,8 @@ public class Test1 {
 		// 2. Submit request via HTTP method to get response
 		Response res = RestAssured.get("/posts/34"); // path parameter
 		// 3. Analyze response
-		String z = res.getBody().asString();
-		System.out.println(z);
+		String hi= res.getBody().asString();
+		System.out.println(hi);
 	}
 }
 	
